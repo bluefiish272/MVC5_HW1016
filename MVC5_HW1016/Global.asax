4d462c4ddeaf419f4d4924d34ca5a3decac289e7
user <user@user-PC>
2016-10-16 21:00:10 +0800

    加入專案檔案。

---
+++ MVC5_HW1016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_HW1016.MvcApplication" Language="C#" %>
---
+++ MVC5_HW1016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5_HW1016.Startup))]
+namespace MVC5_HW1016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
